f2dca9aeaafcd0290468623af1e1dfac0f9fc87b
Vladislav Murzin <vlad99samara@mail.ru>
2023-03-11 17:23:44 +0400

    add RTK Query

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,68 +1,54 @@
-import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
-import {useHttp} from '../../hooks/http.hook';
+// import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+// import {useHttp} from '../../hooks/http.hook';
 
 
-const heroesAdapter = createEntityAdapter();
+// const heroesAdapter = createEntityAdapter();
 
-// const initialState = {
-//     heroes: [],
+// const initialState = heroesAdapter.getInitialState({
 //     heroesLoadingStatus: 'idle'
-// }
+// });
 
-const initialState = heroesAdapter.getInitialState({
-    heroesLoadingStatus: 'idle'
-});
+// export const fetchHeroes = createAsyncThunk(
+//     'heroes/fetchHeroes',
+//     () => {
+//         const {request} = useHttp();
+//         return request("http://localhost:3001/heroes")
+//     }
+// );
 
-export const fetchHeroes = createAsyncThunk(
-    'heroes/fetchHeroes',
-    () => {
-        const {request} = useHttp();
-        return request("http://localhost:3001/heroes")
-    }
-);
+// const heroesSlice = createSlice({
+//     name: 'heroes',
+//     initialState,
+//     reducers: {
+//         heroCreated: (state, action) => {
+//             heroesAdapter.addOne(state, action.payload);
+//         },
+//         heroDeleted: (state, action) => {
+//             heroesAdapter.removeOne(state, action.payload);
+//         }
+//     },
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+//             .addCase(fetchHeroes.fulfilled, (state, action) => {
+//                 state.heroesLoadingStatus = 'idle';
+//                 heroesAdapter.setAll(state, action.payload);
+//             })
+//             .addCase(fetchHeroes.rejected, state => {state.heroesLoadingStatus = 'error'})
+//             .addDefaultCase(() => {})
+//     }
+// });
 
-const heroesSlice = createSlice({
-    name: 'heroes',
-    initialState,
-    reducers: {
-        // heroesFetching: state => {state.heroesLoadingStatus = 'loading'},
-        // heroesFetched: (state, action) => {
-        //     state.heroesLoadingStatus = 'idle';
-        //     state.heroes = action.payload;
-        // },
-        // heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
-        heroCreated: (state, action) => {
-            // state.heroes.push(action.payload);
-            heroesAdapter.addOne(state, action.payload);
-        },
-        heroDeleted: (state, action) => {
-            // state.heroes = state.heroes.filter(item => item.id !== action.payload);
-            heroesAdapter.removeOne(state, action.payload);
-        }
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
-            .addCase(fetchHeroes.fulfilled, (state, action) => {
-                state.heroesLoadingStatus = 'idle';
-                // state.heroes = action.payload
-                heroesAdapter.setAll(state, action.payload);
-            })
-            .addCase(fetchHeroes.rejected, state => {state.heroesLoadingStatus = 'error'})
-            .addDefaultCase(() => {})
-    }
-});
+// const {actions, reducer} = heroesSlice;
 
-const {actions, reducer} = heroesSlice;
+// export default reducer;
 
-export default reducer;
+// export const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
 
-export const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
-
-export const {
-    heroesFetching,
-    heroesFetched,
-    heroesFetchingError,
-    heroCreated,
-    heroDeleted
-} = actions;
+// export const {
+//     heroesFetching,
+//     heroesFetched,
+//     heroesFetchingError,
+//     heroCreated,
+//     heroDeleted
+// } = actions;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import heroes from '../components/heroesList/heroesSlice';
+// import heroes from '../components/heroesList/heroesSlice';
 import filters from '../components/heroesFilters/filtersSlice';
+import { apiSlice } from '../api/apiSlice';
 
 
 const stringMiddleware = () => (next) => (action) => {
@@ -20,8 +21,10 @@ const stringMiddleware = () => (next) => (action) => {
 
 
 const store = configureStore({
-    reducer: {heroes, filters},
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    reducer: {
+        filters, 
+        [apiSlice.reducerPath]: apiSlice.reducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production'
 })
 
